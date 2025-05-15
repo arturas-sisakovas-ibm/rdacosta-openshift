fdc6fd1beeee9e2ae072fe5743567d314cfb14a5
Student User <student@workstation.lab.example.com>
2025-05-15 06:11:40 -0400

    Refactor scripts, add new demo

--- admin/backup-pvc.sh
+++ admin/backup-pvc.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <is>"
+  echo "Usage: $0 <pvc_name>"
   exit 1
 fi
 
-oc get is "$1" -o yaml \
+oc get pvc "$1" -o yaml \
 	| yq d - metadata.annotations \
 	| yq d - metadata.creationTimestamp \
+	| yq d - metadata.finalizers \
 	| yq d - metadata.namespace \
 	| yq d - metadata.resourceVersion \
 	| yq d - metadata.uid \
-	| yq d - metadata.generation \
+	| yq d - spec.volumeName \
 	| yq d - status
 
