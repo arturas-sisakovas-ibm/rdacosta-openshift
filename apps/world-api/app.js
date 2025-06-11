5417c7fdad5f0a79398c26d2e37688c908dbda16
Ricardo da Costa <rgdacosta@gmail.com>
2025-06-11 17:29:10 +0200

    Remove promise

--- apps/world-api/app.js
+++ apps/world-api/app.js
@@ -2,7 +2,7 @@
 
 const express           = require('express');
 const cors              = require('cors');
-const mysql             = require('mysql2/promise');
+const mysql             = require('mysql2');
 const cityCountryRoutes = require('./routes/cityCountry');
 const healthzRoute      = require('./routes/healthz');
 
