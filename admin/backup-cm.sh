a18da52ea36ba26ff71f27bf5cb683555ec33791
Ricardo da Costa <rgdacosta@gmail.com>
2025-06-12 07:35:37 -0400

    Implement fe as part of declarative app stack

---
+++ admin/backup-cm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <cm_name>"
+  exit 1
+fi
+
+oc get cm "$1" -o yaml \
+	| yq d - metadata.annotations \
+	| yq d - metadata.creationTimestamp \
+	| yq d - metadata.namespace \
+	| yq d - metadata.resourceVersion \
+	| yq d - metadata.uid \
+	| yq d - status
+
