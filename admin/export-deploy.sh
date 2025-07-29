b20345769b648d78d508124d46f402916a64ca72
Student User <student@workstation.lab.example.com>
2025-07-29 13:58:43 -0400

    Rename export files, fix deploy_fe.yaml image

--- admin/export-deploy.sh
+++ admin/export-deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <deployment>"
+  echo "Usage: $0 <backup>"
   exit 1
 fi
 
-oc get deploy "$1" -o yaml \
+oc get backup "$1" -o yaml \
 	| yq d - metadata.annotations \
+	| yq d - metadata.managedFields \
 	| yq d - metadata.creationTimestamp \
 	| yq d - metadata.namespace \
 	| yq d - metadata.resourceVersion \
 	| yq d - metadata.uid \
 	| yq d - metadata.generation \
 	| yq d - spec.template.metadata.annotations \
-	| yq d - status \
-	| sed 's/@sha.*//'
-
+	| yq d - status
--- admin/export-restore.sh
+++ admin/export-restore.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <cm_name>"
+  echo "Usage: $0 <restore>"
   exit 1
 fi
 
-oc get cm "$1" -o yaml \
+oc get restore "$1" -o yaml \
 	| yq d - metadata.annotations \
+	| yq d - metadata.finalizers \
+	| yq d - metadata.managedFields \
 	| yq d - metadata.creationTimestamp \
 	| yq d - metadata.namespace \
 	| yq d - metadata.resourceVersion \
 	| yq d - metadata.uid \
+	| yq d - metadata.generation \
+	| yq d - spec.template.metadata.annotations \
 	| yq d - status
-
