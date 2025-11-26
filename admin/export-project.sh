d004fa0c5e3f82fadd66e9b0ffa73c8407d28ab5
Ricardo da Costa <rgdacosta@gmail.com>
2025-11-26 14:45:33 +0400

    Add new shell script

---
+++ admin/export-project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <project>"
+  exit 1
+fi
+
+oc get is "$1" -o yaml \
+	| yq d - metadata.annotations \
+	| yq d - metadata.creationTimestamp \
+	| yq d - metadata.labels \
+	| yq d - metadata.resourceVersion \
+	| yq d - metadata.uid \
+    | yq d - spec \
+	| yq d - status \
+    | sed -ie 's/Project/ProjectRequest'
+
