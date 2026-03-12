67a37c266c1218ccba4ffbe9290f420919090317
Ricardo da Costa <rgdacosta@gmail.com>
2026-03-12 09:24:56 -0400

    Parameterise ap

--- apps/nodejs-helloworld/app.js
+++ apps/nodejs-helloworld/app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 app = express();
 
+const app_ver = process.env.app_ver || "unknown";
+
 app.get('/', function (req, res) {
-  res.send('Hello World! I am version 1.0');
+  res.send(`Hello World! I am version ${app_ver}`);
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log(`Example app listening on port 8080! Version ${app_ver}`);
 });
-
